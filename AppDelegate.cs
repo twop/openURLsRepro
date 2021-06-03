90f14103f16a83eac0c41205a8db1525a5654571
Simon Korzunov <twop@hey.com>
2021-06-02 17:35:13 -0700

    initial commit

---
+++ AppDelegate.cs
@@ -0,0 +1,29 @@
+﻿using AppKit;
+using Foundation;
+
+namespace openURLsTest
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : NSApplicationDelegate
+    {
+        public AppDelegate()
+        {
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            // Insert code here to initialize your application
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+
+        
+        public override void OpenUrls(NSApplication application, NSUrl[] urls)
+        {
+            throw new System.Exception("opened with: " + urls[0].AbsoluteString);
+        }
+    }
+}
